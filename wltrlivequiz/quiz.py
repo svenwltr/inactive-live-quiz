8ad241fa13307902bde07ff73cf41e4f7ce12390
Sven Walter <sven@wltr.eu>
2012-11-20 00:39:58 +0100

    Added functionallity for startup events (i.e. send team_update on ready).

--- wltrlivequiz/quiz.py
+++ wltrlivequiz/quiz.py
@@ -5,6 +5,10 @@ from websocket import QuizWebSocket
 
 
 class Quiz(object):
+
+    def __init__(self):
+        self.startup_events = {};
+        
     
     def recv(self, ws, event, data):
         mname = "on_%s" % event.replace('.', '_')
@@ -16,12 +20,17 @@ class Quiz(object):
             method(ws, data)
     
     
+    def on_session_ready(self, ws, data):
+        for event, data in self.startup_events.items():
+            ws.send(event, data)
+    
+    
     def on_setup_form_update(self, ws, data):
-        
         teams = [t for t in data['teams'] if t] # remove empty ones
         teams.sort()
         
         QuizWebSocket.send_all("data.team_update", teams)
+        self.startup_events["data.team_update"] = teams
 
 
 
